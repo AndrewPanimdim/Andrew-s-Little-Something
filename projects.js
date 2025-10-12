d0457ccb751ade189eda5a1f0cbc6631f42cd373
Andrew Panimdim <baelmeans74@gmail.com>
2025-10-12 16:04:10 +0800

    a

--- projects.js
+++ projects.js
@@ -1,31 +1,18 @@
-
-const minichar = document.getElementById('minicharacterdd');
+const minichar = document.getElementById('minicharacter');
+const moveeffect = document.getElementById('moveEffect');
 
 let charx = 2;
 let chary = 0
 const stepmini = 3;
-let ministatus = false;
 
 stationary = true;
 
-moveeffect.addEventListener('click', () => {
-    ministatus = !ministatus;
-    if (ministatus) {
-        minichar.style.display = 'block';
-        hello.style.color = 'white';
-        document.body.style.backgroundColor = 'rgba(15, 15, 15, 1)';
-        background.style.display = 'none';
-        darkmode.style.backgroundColor = 'rgb(255, 255, 255)';
-    } else {
-        minichar.style.display = 'none';
-    }
-});
 
 document.addEventListener('keydown', (event) => {
     if(event.key === 'ArrowRight' || event.key === 'd' || event.key === 'D'){
         charx += stepmini;
         if(charx > 103) {
-            window.open('https://andrewpanimdim.github.io/Andrew-s-Little-Something/projects.html', '_blank');
+            charx = -5
         }
         minichar.style.left = charx + '%';
         stationary = false;
@@ -48,3 +35,26 @@ document.addEventListener('keyup', (event) => {
         minichar.src = 'gif/standing.gif';
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
